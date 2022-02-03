252209d3a5a1310a5f871c9fbd359a96a058ec96
Danial <imdaniald@gmail.com>
2022-02-04 00:53:55 +0330

    Add class template

---
+++ src/ClassTemplate.js
@@ -0,0 +1,55 @@
+/**
+ * a class template for future classes implementations
+ * should be improved to be more generic
+ */
+
+class ClassTemplate {
+  constructor(options) {
+    this.DOM = {}
+    this.init()
+  }
+
+  init() {
+    this.x = 0
+    this.y = 0
+    this.dx = Math.random()
+    this.dy = Math.random()
+    this.active = true
+  }
+
+  destroy() {
+    this.active = false
+  }
+
+  reset() {}
+
+  // add event listeners
+  addEvents() {}
+  removeEvents() {}
+
+  // or just an init event
+  initEvents() {}
+
+  run() {
+    this.addEvents()
+  }
+
+  // pause animation and events
+  pause() {
+    this.removeEvents()
+  }
+
+  createDom() {
+    this.DOM.el = document.createElement('div')
+  }
+
+  // update Dom UI
+  updateDom() {}
+
+  /**
+   * @return {Boolean}
+   */
+  canDoSomething() {}
+
+  doSomething() {}
+}
